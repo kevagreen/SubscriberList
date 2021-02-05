854204a339fb485fcfa21998eb9bf676ff447807
kevarey reynolds <kevareynolds@gmail.com>
2021-02-05 09:59:26 -0600

    updated SubscriberController, Subscriber, linked Controller to to index, added attributes

---
+++ subscriberList/src/main/java/com/tts/subscriberList/subscriber/SubscriberRepository.java
@@ -0,0 +1,6 @@
+package com.tts.subscriberList.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+}
